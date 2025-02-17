8d35560b66a6a1d93f821c9157f6f67f60d02822
samgrim2024 <samgrim2024@gmail.com>
2025-02-17 01:57:18 +0000

    Add setup.py for package configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gdpr-obfuscator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "boto3"
+    ],
+    description="A package to obfuscate PII data in CSV files stored in S3.",
+    python_requires='>=3.6',
+)
