c2b671d87a519d4ac63bcbe526852154ccdbe2cb
samgrim2024 <samgrim2024@gmail.com>
2025-02-17 00:47:16 +0000

    Add parameter documentation for read_csv_from_s3 function

--- gdpr_obfuscator/s3_handler.py
+++ gdpr_obfuscator/s3_handler.py
@@ -4,7 +4,11 @@ import pandas as pd
 def read_csv_from_s3(bucket_name, file_key):
     """
     Reads a CSV file from AWS S3 and returns a Pandas DataFrame.
-    
+
+    :param bucket_name: The name of the S3 bucket.
+    :param file_key: The key of the file in the S3 bucket.
+
+    :return: A Pandas DataFrame.
     """
     s3 = boto3.client('s3')
     
